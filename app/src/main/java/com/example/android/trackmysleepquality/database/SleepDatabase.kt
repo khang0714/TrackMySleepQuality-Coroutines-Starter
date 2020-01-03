3e79afba4ffdcf88b9880a5899e380bd86e37e40
khang0714 <khang0714@yahoo.com>
2020-01-03 22:45:23 +0800

    Coroutines and Room

--- app/src/main/java/com/example/android/trackmysleepquality/database/SleepDatabase.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/database/SleepDatabase.kt
@@ -25,6 +25,7 @@ abstract class SleepDatabase : RoomDatabase() {
     abstract val sleepDatabaseDao: SleepDatabaseDao
 
     companion object {
+
         @Volatile
         private var INSTANCE: SleepDatabase? = null
 
